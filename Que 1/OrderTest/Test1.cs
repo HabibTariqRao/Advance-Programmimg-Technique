32c585fc5273521080201514b68d649229464fde
HabibTariqRao <habibtariqrao@gmail.com>
2026-04-07 13:52:55 +0100

    Added new folder for Churros testing

---
+++ Que 1/OrderTest/Test1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class OrderTests
+{
+    [TestMethod]
+    public void PayBill_ShouldReturnCorrectAmount()
+    {
+        // Arrange
+        Order order = new Order("Chocolate", 2, 8);
+
+        // Act
+        double result = order.PayBill();
+
+        // Assert
+        Assert.AreEqual(16, result);
+    }
+}
